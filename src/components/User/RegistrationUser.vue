af24f0a3ae8221d2129027e8ce7e2b5c0b523437
jortimar <javier.orti-marina@altran.com>
2024-04-10 18:21:14 +0200

    fix: form Product & List Product

--- src/components/User/RegistrationUser.vue
+++ src/components/User/RegistrationUser.vue
@@ -48,6 +48,7 @@ export default {
                 this.email = "";
                 this.password = "";
                 console.log(response.data)
+                alert('Usuario creado')
               }
             })
             .catch(error => {
